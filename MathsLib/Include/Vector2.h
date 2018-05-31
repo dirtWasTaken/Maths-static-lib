90e12fec091c6b1a54af0904e41bbefe154b0304
Mikey Derbyshire <mikeyderbyshire@hotmail.com>
2018-05-31 13:35:26 +1000

    Reformatting most of the layout of the vector 2 class, doing Vector2 right the first time so that subsequent Vector3 vector4 classes will be easier to reproduce based off this class

--- MathsLib/Include/Vector2.h
+++ MathsLib/Include/Vector2.h
@@ -1,27 +1,43 @@
-#pragma once
+#ifndef MHD_VECTOR2
+#define MHD_VECTOR2
+
 class Vector2
 {
 public:
-
+	//constructors
 	Vector2();
 
 	virtual ~Vector2();
 
 	struct Vec2
 	{
-		float X = 0.0;
-		float Y = 0.0;
+		float x = 0.0;
+		float y = 0.0;
+		Vec2(const Vec2 & v) : x(v.x), y(v.y) {}
+		Vec2(const Vec2 * v) : x(v->x), y(v->y) {}
 	};
 
-	Vec2 operator + (Vec2 & other);
-	Vec2 operator - (Vec2 & other);
-	Vec2 operator * (float rhs);
-	float pointC(float C_X, float C_Y);
-
-	float magnitudeCalculator(float abs);
+	
 
 
-	void DotProduct();
+	
+	//methods
+	float length() const;
+	float lengthSquared() const;
+	float distance(const Vec2 & v) const;
+	float distanceSquared(const Vec2 & v) const;
+	float DotProduct(const Vec2 & v) const;
+	float crossProduct(const Vec2 & v) const;
+
+
+	Vector2 & normal();
+	Vector2 & normalise();
+
+	//Assignment and equality operators
+	inline Vec2 operator = (const Vec2 & v) 
+	{ 
+	}
 
 };
+#endif
 
--- MathsLib/Vector2.cpp
+++ MathsLib/Vector2.cpp
@@ -3,89 +3,80 @@
 #include <iostream>
 
 
-Vector2::Vector2()
+Vector2::Vector2() {}
+
+Vector2::~Vector2() {}
+
+//METHODS
+float Vector2::length() const
+{
+}
+float Vector2::lengthSquared() const
+{
+}
+float Vector2::distance(const Vec2 & v) const
+{
+}
+float Vector2::distanceSquared(const Vec2 & v) const
+{
+}
+float Vector2::DotProduct(const Vec2 & v) const
+{
+}
+float Vector2::crossProduct(const Vec2 & v) const 
+{
+}
+Vector2 & Vector2::normal()
+{
+}
+Vector2 & Vector2::normalise()
 {
 }
 
 
-Vector2::~Vector2()
-{
-}
 
 
-float Vector2::pointC(float C_X, float C_Y)
-{
-	std::vector<Vec2> V1(2);
-	if (V1[0].Y > V1[1].Y)
-	{
-		C_Y = V1[1].Y;
-		return C_Y;
-	}
-		else 
-		{
-			C_Y = V1[0].Y;
-			return C_Y;
-		}
-	if (V1[0].X > V1[1].X)
-	{
-		C_X = V1[1].X;
-		return C_X;
-	}
-		else
-		{
-			C_X = V1[0].X;
-			return C_X;
-		}
-
-}
-
-float Vector2::magnitudeCalculator(float abs)
-{
-
-}
-
-Vector2::Vec2 Vector2::operator * (float rhs)
-{
-	Vec2 temp;
-	temp.X = temp.X * rhs;
-	temp.Y = temp.Y * rhs;
-	return temp;
-}
-
-//pythag: c*2(length between two vector2) =   + (ylength*2) 
-//C = sqroot of c*2
-
-Vector2::Vec2 Vector2::operator + (Vec2 & other)
-{
-	Vec2 temp;
-	temp.X = temp.X + other.X;
-	temp.Y = temp.X + other.X;
-	return temp;
-}
-
-Vector2::Vec2 Vector2::operator - (Vec2 & other)
-{
-	Vec2 temp;
-	temp.X = temp.X - other.X;
-	temp.Y = temp.X - other.X;
-	return temp;
-}
 
 
-float Vector2::magnitudeCalculator(float abs)
-{
-	
-	return abs;
-}
-
-float Vector2::pointC(float C_X, float C_Y)
-{
-	return 0;
-}
 
 
-void Vector2::DotProduct()
-{
 
-}
 
+
+
+//
+//float Vector2::pointC(float C_X, float C_Y)
+//{
+//}
+//float Vector2::magnitudeCalculator(float abs)
+//{
+//}
+
+//Vector2::Vec2 Vector2::operator * (float rhs)
+//{
+//	Vec2 temp;
+//	temp.X = temp.X * rhs;
+//	temp.Y = temp.Y * rhs;
+//	return temp;
+//}
+//
+//Vector2::Vec2 Vector2::operator + (Vec2 & other)
+//{
+//	Vec2 temp;
+//	temp.X = temp.X + other.X;
+//	temp.Y = temp.X + other.X;
+//	return temp;
+//}
+//
+//Vector2::Vec2 Vector2::operator - (Vec2 & other)
+//{
+//	Vec2 temp;
+//	temp.X = temp.X - other.X;
+//	temp.Y = temp.X - other.X;
+//	return temp;
+//}
+//
+//void Vector2::DotProduct()
+//{
+//
+//}
