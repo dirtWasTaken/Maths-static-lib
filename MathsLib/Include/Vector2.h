10aee5fc79b9d5ebe0bb31be92f179aadafe90c7
Mikey Derbyshire <mikeyderbyshire@hotmail.com>
2018-05-31 15:52:14 +1000

    Started on the functionality of the reformatted class for vector2, hopefully have it fully working tomorrow ready to move onto my vector 3 class

--- MathsLib/Include/Vector2.h
+++ MathsLib/Include/Vector2.h
@@ -4,33 +4,95 @@
 class Vector2
 {
 public:
+	float x;
+	float y;
 	//constructors
-	Vector2();
-
-	virtual ~Vector2();
-
-	struct Vec2
-	{
-		float x = 0.0;
-		float y = 0.0;
-		Vec2* v = nullptr;
-	};
+	Vector2 (void);
+    Vector2 (float xValue = 0.0, float yValue = 0.0);
+	Vector2 (const Vector2 & v);
+	Vector2 (const Vector2 * v);
+	~Vector2 (void);
 
 	//methods
-	float length() const;
-	float lengthSquared() const;
-	float distance(const Vec2 & v) const;
-	float distanceSquared(const Vec2 & v) const;
-	float dotProduct(const Vec2 & v) const;
-	float crossProduct(const Vec2 & v) const;
+	void set(float xValue, float yValue);
+
+	float magnitude() const;
+	float magnitudeSquared() const;
+	float distance(const Vector2 & v) const;
+	float distanceSquared(const Vector2 & v) const;
+	float dotProduct(const Vector2 & v) const;
+	float crossProduct(const Vector2 & v) const;
+
 	Vector2 & normal();
 	Vector2 & normalise();
 
-	//Assignment and equality operators
-	inline Vec2 operator = (const Vec2 & v) 
-	{ 
-	}
+	//ASSINGMENT AND EQUALITY OPERATIONS
+	inline Vector2 & operator = (const Vector2 & v) 
+	{ x = v.x; y = v.y; return *this; }
 
+	inline Vector2 & operator = (const float & f) 
+	{ x = f; y = f; return *this; }
+
+	inline Vector2 & operator - (void) 
+	{ x = -x; y = -y; return *this; }
+
+	inline bool operator == (const Vector2 & v) const 
+	{ return (x == v.x) && (y == v.y); }
+
+	inline bool operator != (const Vector2 & v) const 
+	{ return (x != v.x) || (y != v.y); }
+
+	//VECTOR2 TO VECTOR2 OPERATIONS
+	inline const Vector2 operator + (const Vector2 & v) const 
+	{ return Vector2(x + v.x, y + v.y); }
+
+	inline const Vector2 operator - (const Vector2 & v) const 
+	{ return Vector2(x - v.x, y - v.y); }
+
+	inline const Vector2 operator * (const Vector2 & v) const
+	{ return Vector2(x * v.x, y * v.y); }
+
+	inline const Vector2 operator / (const Vector2 & v) const 
+	{ return Vector2(x / v.x, y / v.y); }
+
+	//VECTOR2 TO THIS OPERATIONS
+	inline Vector2 & operator += (const Vector2 & v) 
+	{ x += v.x; y += v.y; return *this; }
+
+	inline Vector2 & operator -= (const Vector2 & v) 
+	{ x -= v.x; y -= v.y; return *this; }
+
+	inline Vector2 & operator *= (const Vector2 & v) 
+	{ x *= v.x; y *= v.y; return *this; }
+
+	inline Vector2 & operator /= (const Vector2 & v) 
+	{ x /= v.x; y /= v.y; return *this; }
+
+	//SCALER TO VECTOR2 OPERATIONS
+	inline const Vector2 operator + (float v) const 
+	{ return Vector2(x + v, y + v); }
+
+	inline const Vector2 operator - (float v) const 
+	{ return Vector2(x - v, y - v); }
+
+	inline const Vector2 operator * (float v) const 
+	{ return Vector2(x * v, y * v); }
+
+	inline const Vector2 operator / (float v) const 
+	{ return Vector2(x / v, y / v); }
+
+	//SCALER TO THIS OPERATIONS
+	inline Vector2 & operator += (float v) 
+	{ x += v; y += v; return *this; }
+
+	inline Vector2 & operator -= (float v) 
+	{ x -= v; y -= v; return *this; }
+
+	inline Vector2 & operator *= (float v)
+	{ x *= v; y *= v; return *this; }
+
+	inline Vector2 & operator /= (float v) 
+	{ x /= v; y /= v; return *this; }
 };
 #endif
 
--- MathsLib/Vector2.cpp
+++ MathsLib/Vector2.cpp
@@ -3,32 +3,41 @@
 #include <iostream>
 
 
-Vector2::Vector2() {}
+Vector2::Vector2 (void) : x(0), y(0) {}
+Vector2::Vector2 (float xValue, float yValue) : x(xValue), y(yValue) { }
+Vector2::Vector2 (const Vector2 & v) : x(v.x), y(v.y) { }
+Vector2::Vector2 (const Vector2 * v) : x(v->x), y(v->y) { }
 
-Vector2::~Vector2() {}
+Vector2::~Vector2(void) {}
 
 ////METHODS
-float Vector2::length() const
+void Vector2::set(float xValue, float yValue)
 {
+	x = xValue;
+	y = yValue;
 }
-float Vector2::lengthSquared() const
+float Vector2::magnitude() const
 {
+	 return sqrt (x * x + y * y);
 }
-float Vector2::distance(const Vec2 & v) const
+float Vector2::distance(const Vector2 & v) const
 {
+	return sqrt (((x - v.x) * (x - v.x)) + ((y - v.y) * (y - v.y)));
 }
-float Vector2::distanceSquared(const Vec2 & v) const
+float Vector2::distanceSquared(const Vector2 & v) const
 {
+	return (((x - v.x) * (x - v.x)) + ((y - v.y) * (y - v.y)));
 }
-float Vector2::dotProduct(const Vec2 & v) const
+float Vector2::dotProduct(const Vector2 & v) const
 {
+	
 }
-float Vector2::crossProduct(const Vec2 & v) const 
+float Vector2::crossProduct(const Vector2 & v) const
 {
+	return 0;
 }
 Vector2 & Vector2::normal()
 {
-
 }
 Vector2 & Vector2::normalise()
 {
